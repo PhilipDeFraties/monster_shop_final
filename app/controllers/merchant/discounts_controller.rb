bd3128564acaae292c215b6e56f5ee3ef290d4c0
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-18 20:51:56 -0600

    Add merchant/discount_controller with index, new, create actions

---
+++ app/controllers/merchant/discounts_controller.rb
@@ -0,0 +1,26 @@
+class Merchant::DiscountsController < Merchant::BaseController
+
+  def index
+    @discounts = current_user.merchant.discounts
+  end
+
+  def new
+  end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to "/merchant/discounts"
+    else
+      generate_flash(discount)
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:minimum_quantity, :discounted_percentage, :discount_name)
+  end
+end
