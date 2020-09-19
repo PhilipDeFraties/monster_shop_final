cc703713bc8515c7f67beded0c7c19c6cffd5d90
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-18 20:52:50 -0600

    Add tests for new discount creation

--- spec/features/merchant/discount_new_spec.rb
+++ spec/features/merchant/discount_new_spec.rb
@@ -7,17 +7,25 @@ RSpec.describe 'New Merchant Discount' do
       @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
     end
-    it "I see a link on my merchant dashboard to make a new discount" do
-      visit '/merchant'
-
-      expect(page).to have_link('New Discount')
-    end
-
-    it 'When I click the new discount link I am taken to a page with a form' do
-      visit '/merchant'
+    it 'When I click the new discount link I am taken to a new discount form' do
+      visit '/merchant/discounts'
       click_link 'New Discount'
 
       expect(current_path).to eq('/merchant/discounts/new')
+    end
+
+    it "I can create a new discount for a merchant" do
+      min_quantity = 20
+      discounted_percentage = 5
+      discount_name = 'Fall Discount'
+
+      visit "/merchant/discounts/new"
+      fill_in 'Minimum quantity', with: min_quantity
+      fill_in "Discounted percentage", with: discounted_percentage
+      fill_in "Discount name", with: discount_name
+      click_button 'Create Discount'
+      expect(current_path).to eq("/merchant/discounts")
+      expect(page).to have_content("Fall Discount")
     end
   end
 end
