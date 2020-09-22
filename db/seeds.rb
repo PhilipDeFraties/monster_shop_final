8e698bdf08af85ea55dbeaa3a31a73601215c126
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-21 23:32:58 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-# 
+#
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
@@ -12,9 +12,13 @@ brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'D
 @ogre = megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
 @giant = megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 @hippo = brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+@hippogriff = megan.items.create!(name: 'Hippogriff', description: "Like a hippo but extra", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 50 )
+
 
 @user = User.create!(name: 'Jade', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'jade@gmail123.com', password: '123')
 @merchant_employee = megan.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan12345@gmail.com', password: 'securepassword', role: 1)
+@merchant_employee2 = brian.users.create(name: 'Brian', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'brian@gmail.com', password: '123', role: 1)
+
 @order_1 = @user.orders.create!(status: "pending")
 @order_2 = @user.orders.create!(status: "pending")
 
