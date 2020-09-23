d8c766af3c2003c6b347687da106f8aa04eaaf8f
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-22 18:13:16 -0600

    Add model tests for new cart methods

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -8,9 +8,15 @@ RSpec.describe Cart do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 2 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @discount_1 = @megan.discounts.create!(minimum_quantity: 5, discounted_percentage: 20, discount_name: "Fall 5")
+      @discount_2 =@megan.discounts.create!(minimum_quantity: 10, discounted_percentage: 50, discount_name: "Fall 10")
       @cart = Cart.new({
         @ogre.id.to_s => 1,
         @giant.id.to_s => 2
+        })
+      @cart_2 = Cart.new({
+        @ogre.id.to_s => 5,
+        @giant.id.to_s => 3
         })
     end
 
@@ -51,6 +57,8 @@ RSpec.describe Cart do
     it '.subtotal_of()' do
       expect(@cart.subtotal_of(@ogre.id)).to eq(20)
       expect(@cart.subtotal_of(@giant.id)).to eq(100)
+      expect(@cart_2.subtotal_of(@giant.id)).to eq(150)
+      expect(@cart_2.subtotal_of(@ogre.id)).to eq(80)
     end
 
     it '.limit_reached?()' do
@@ -62,6 +70,21 @@ RSpec.describe Cart do
       @cart.less_item(@giant.id.to_s)
 
       expect(@cart.count_of(@giant.id)).to eq(1)
+    end
+
+    it '.find_discount' do
+    expect(@cart_2.find_discount(@ogre.id, 5)).to eq(@discount_1)
+    expect(@cart_2.find_discount(@giant.id, 10)).to eq(@discount_2)
+    end
+
+    it '.apply_discount' do
+      expect(@cart_2.apply_discount(@ogre.id, @discount_1.discounted_percentage)).to eq(16.0)
+      expect(@cart_2.apply_discount(@giant.id, @discount_2.discounted_percentage)).to eq(25)
+    end
+
+    it '.apply_discount' do
+      expect(@cart_2.adjust_price(@ogre.id)).to eq(16)
+      expect(@cart_2.adjust_price(@giant.id)).to eq(50)
     end
   end
 end
