455e2f1bf76c93eaa3b0244d81393d3f265add86
Philip DeFraties <philipjfry338@gmail.coom>
2020-09-22 18:20:19 -0600

    Edit name of method called

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -37,7 +37,7 @@ class Cart
   end
 
   def subtotal_of(item_id)
-    @contents[item_id.to_s] * adjusted_price(item_id)
+    @contents[item_id.to_s] * adjust_price(item_id)
   end
 
   def limit_reached?(item_id)
@@ -48,11 +48,11 @@ class Cart
     @discount = Item.find(item_id).discounts.where('minimum_quantity <= ?', quantity).order(:minimum_quantity).last
   end
 
-  def apply_discount(item_id, discount)
-    ((100 - discount) * 0.01) * Item.find(item_id).price
+  def apply_discount(item_id, discounted_percentage)
+    ((100 - discounted_percentage) * 0.01) * Item.find(item_id).price
   end
 
-  def adjusted_price(item_id)
+  def adjust_price(item_id)
     if find_discount(item_id, count_of(item_id))
       apply_discount(item_id, @discount.discounted_percentage)
     else
